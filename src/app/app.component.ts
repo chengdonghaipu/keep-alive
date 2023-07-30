9c19a6664641ec6d8de03617565f854afb1956eb
chendong <1296942311@qq.com>
2023-07-31 00:10:02 +0800

    feat(init): init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['./app.component.scss'],
+  template: ``
+})
+export class AppComponent {
+  title = 'keep-alive';
+}
